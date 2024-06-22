5c6c5cb39bfb70db87783ee0848fe89f5ac37e99
Beloved <aycahanim@gmail.com>
2024-06-22 12:56:14 +0300

    aaa

---
+++ view/euro2024.php
@@ -0,0 +1,36 @@
+<div class="main-template main-template--quiz main-template--fixed-header">
+   <div class="main-template__container" style="padding-top: 68px;">
+      <div class="styles__Page-sc-1ie172w-0 cQnDrJ">
+         <div class="styles__Header-sc-1ie172w-1 dPQAPJ">
+            <div class="styles__HeaderBody-sc-1ie172w-2 hxvjGA">
+               <span class="styles__HeaderLive-sc-1ie172w-3 jxCPRV">Canlı</span>
+               <div class="styles__HeaderTitle-sc-1ie172w-4 cxKkdL">TahmineGel!
+                  EURO 2024
+               </div>
+               <div class="styles__HeaderDesc-sc-1ie172w-5 hkQddu">Her gün ücretsiz Freebet!</div>
+            </div>
+         </div>
+         <div class="styles__Body-sc-1ie172w-6 kZyTyw">
+            <h2 class="styles__BodyTitle-sc-1ie172w-7 hFBWjr">
+               <h2 class="styles__Title-sc-1ie172w-8 dRQvkO">EURO 2024</h2>
+            </h2>
+            <div class="styles__Content-sc-1ie172w-10 gmjMMU">
+               <div class="styles__Text-sc-1weez00-0 guKiWO styles__Text-sc-1ie172w-9 jPulnJ">
+                  <h4>Kurallar ve Şartlar</h4>
+                  <ol>
+                     <li>Tahmin yarışmasına katılım ücretsizdir.</li>
+                     <li>Her gün giriş yaparak ertesi güne ait maçlara dair tahminlerde bulunun.</li>
+                     <li>Doğru cevap sayısına göre ücretsiz bahis kazanın:<br><br>3 Doğru&nbsp;cevap: 50 TRY Bedava Bahis<br>4 Doğru&nbsp;cevap: 100 TRY Bedava Bahis<br>5 Doğru cevap: 200 TRY Bedava Bahis<br><br></li>
+                     <li>Tahminler 90 dakika sonuçları üzerinden değerlendirilecektir.</li>
+                     <li>Kazanan tahminlerinize ait ödüller maçlar sonuçlandıktan sonra 24 saat içinde hesabınıza eklenmiş olacaktır.</li>
+                     <li>14 Haziran - 14 Temmuz Arası oynanacak Euro 2024 karşılaşmalarında geçerlidir.</li>
+                  </ol>
+               </div>
+            </div>
+            <div class="styles__Sibebar-sc-1ie172w-11 btVVKg">
+               <div class="styles__RoundInfo-sc-1ie172w-12 jmCAHO">Tur <span class="styles__Bold-sc-1ie172w-19 bnDmUB">10</span> / <span class="styles__Bold-sc-1ie172w-19 bnDmUB">22</span></div>
+               <button class="btn btn-lg btn-secondary styles__StartButton-sc-1ie172w-13 gTstvj" type="button"><span class="btn__label">Başla!</span></button><button class="btn btn-lg btn-transparent-bordered styles__HistoryButton-sc-1ie172w-14 eWeqeg" type="button"><span class="btn__label">Önceki turlar</span></button>
+            </div>
+         </div>
+      </div>
+   </div>
